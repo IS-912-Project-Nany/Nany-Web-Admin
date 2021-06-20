98785ec6360ba91c642c6f7e766b4a38f7ee28fa
kevinoskar <47488256+kevinoskar@users.noreply.github.com>
2021-06-20 11:47:28 -0600

    [FIX] Details in html

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,11 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,} from '@angular/core';
+import { BoxOptionsComponent } from './box-options/box-options.component';
 
 @Component({
   selector: 'app-login',
-  templateUrl: './login.component.html'
+  template: `<app-box-options></app-box-options>`
 })
 export class LoginComponent implements OnInit {
-
   constructor() { }
 
   ngOnInit(): void {
