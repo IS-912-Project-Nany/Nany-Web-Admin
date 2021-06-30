75e3aaa6d9dfbdb540b5846d638f41df69802ef8
franciaavila28 <47584146+franciaavila28@users.noreply.github.com>
2021-06-30 11:46:54 -0600

    [ADD] Order service and model

---
+++ src/app/shared/model/order.ts
@@ -0,0 +1,10 @@
+export interface Order {
+    id?: string;
+    numberOrder?: number;
+    nameProduct?: string;
+    totalOrder?: number;
+    date?: Date;
+    nameClient?: string;
+    ubication?: string;
+    nameCarrier?: string;
+}
---
+++ src/app/shared/services/order.service.ts
@@ -0,0 +1,19 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+import { Order } from '../model/order';
+@Injectable()
+export class OrderService {
+
+  constructor(private http: HttpClient) {}
+
+  getOrders() {
+    return this.http
+      .get<any>('')
+      .toPromise()
+      .then((res) => <Order[]>res.data)
+      .then((data) => {
+        return data;
+      });
+  }
+}
