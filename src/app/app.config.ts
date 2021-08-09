725f4828d05cfaf9c5472dff4e3343160b1dd862
kevinoskar <47488256+kevinoskar@users.noreply.github.com>
2021-08-09 14:46:34 -0600

    [ADD] Angular Env Requeriment

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,1 +1,3 @@
-export const API_BASE_URL = process.env.ROOT_API || 'http://localhost:8888';
+import { environment } from '../environments/environment';
+
+export const API_BASE_URL = environment.ROOT_API || 'http://localhost:8888';
