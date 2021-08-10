eb0bdd75ae9f06c3c9def641aa9a92094e4133f5
kevinoskar <47488256+kevinoskar@users.noreply.github.com>
2021-08-09 19:05:22 -0600

    [ADD] Productos Service

--- src/app/services/productos.service.ts
+++ src/app/services/productos.service.ts
@@ -1,9 +1,60 @@
+import { API_BASE_URL } from '../app.config';
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ProductosService {
+  constructor(private httpClient: HttpClient) {}
 
-  constructor() { }
+  crearProducto(
+    idCategoria: String,
+    idEmpresa: String,
+    producto: any
+  ): Observable<any> {
+    return this.httpClient.post(
+      `${API_BASE_URL}/categorias/${idCategoria}/empresas/${idEmpresa}/productos`,
+      producto
+    );
+  }
+
+  obtenerProductos(idCategoria: String, idEmpresa: String): Observable<any> {
+    return this.httpClient.get(
+      `${API_BASE_URL}//categorias/${idCategoria}/empresas/${idEmpresa}/productos`
+    );
+  }
+
+  obtenerProducto(
+    idCategoria: String,
+    idEmpresa: String,
+    idProducto: String
+  ): Observable<any> {
+    return this.httpClient.get(
+      `${API_BASE_URL}/categorias/${idCategoria}/empresas/${idEmpresa}/productos/${idProducto}`
+    );
+  }
+
+  actualizarProducto(
+    idCategoria: String,
+    idEmpresa: String,
+    idProducto: String,
+    producto: any
+  ): Observable<any> {
+    return this.httpClient.put(
+      `${API_BASE_URL}/categorias/${idCategoria}/empresas/${idEmpresa}/productos/${idProducto}`,
+      producto
+    );
+  }
+
+  borrarProducto(
+    idCategoria: String,
+    idEmpresa: String,
+    idProducto: String
+  ): Observable<any> {
+    return this.httpClient.delete(
+      `${API_BASE_URL}/categorias/${idCategoria}/empresas/${idEmpresa}/productos/${idProducto}`
+    );
+  }
 }
