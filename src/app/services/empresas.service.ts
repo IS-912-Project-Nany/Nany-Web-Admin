a9e62425edd18e0a770f1c79ecf2a88e627791a1
kevinoskar <47488256+kevinoskar@users.noreply.github.com>
2021-08-09 19:04:59 -0600

    [ADD] Empresas Service

--- src/app/services/empresas.service.ts
+++ src/app/services/empresas.service.ts
@@ -1,9 +1,53 @@
+import { API_BASE_URL } from '../app.config';
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class EmpresasService {
+  constructor(private httpClient: HttpClient) {}
 
-  constructor() { }
+  // Obtener categorias
+  obtenerCategorias(): Observable<any> {
+    return this.httpClient.get(`${API_BASE_URL}/categorias`);
+  }
+  
+  // CRUD Empresas
+  crearEmpresa(idCategoria: String, empresa: any): Observable<any> {
+    return this.httpClient.post(
+      `${API_BASE_URL}/categorias/${idCategoria}/empresas`,
+      empresa
+    );
+  }
+
+  obtenerEmpresas(idCategoria: String): Observable<any> {
+    return this.httpClient.get(
+      `${API_BASE_URL}/categorias/${idCategoria}/empresas`
+    );
+  }
+
+  obtenerEmpresa(idCategoria: String, idEmpresa: String): Observable<any> {
+    return this.httpClient.get(
+      `${API_BASE_URL}/categorias/${idCategoria}/empresas/${idEmpresa}`
+    );
+  }
+
+  actualizarEmpresa(
+    idCategoria: String,
+    idEmpresa: String,
+    empresa: any
+  ): Observable<any> {
+    return this.httpClient.put(
+      `${API_BASE_URL}/categorias/${idCategoria}/empresas/${idEmpresa}`,
+      empresa
+    );
+  }
+
+  borrarEmpresa(idCategoria: String, idEmpresa: String): Observable<any> {
+    return this.httpClient.delete(
+      `${API_BASE_URL}/categorias/${idCategoria}/empresas/${idEmpresa}`
+    );
+  }
 }
