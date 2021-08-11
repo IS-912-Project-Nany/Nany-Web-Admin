8382ce629bf585ed2189a9cf5abef1f857abed0e
franciaavila28 <47584146+franciaavila28@users.noreply.github.com>
2021-08-11 00:38:03 -0600

    [ADD] Service and model Motoristas

---
+++ src/app/models/motorista.ts
@@ -0,0 +1,8 @@
+export interface Motorista {
+    _id?: string;
+    motocicleta?: boolean;
+    licencia?: boolean;
+    datosMoviles?: boolean;
+    experiencia?: string;
+    estadoAdmision?: boolean;
+}
--- src/app/services/motoristas.service.ts
+++ src/app/services/motoristas.service.ts
@@ -1,9 +1,26 @@
+import { API_BASE_URL } from '../app.config';
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MotoristasService {
 
-  constructor() { }
+  constructor(private httpClient: HttpClient) { }
+
+  obtenerMotoristas(idMotorista: String):Observable<any>{
+    return this.httpClient.get(`${API_BASE_URL}/usuarios/motoristas/${idMotorista}`);
+  }
+
+  //Actualizar el estado de un motorista
+  actualizarMotorista(idMotorista: String, estado: any):Observable<any>{
+    return this.httpClient.put(`${API_BASE_URL}/usuarios/motoristas/${idMotorista}`, estado);
+  }
+
+  eliminarMotorista(idMotorista: String):Observable<any>{
+    return this.httpClient.delete(`${API_BASE_URL}/usuarios/motoristas/${idMotorista}`);
+  }
+
 }
