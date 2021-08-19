5580dd6069ea9b208ccccc599102bff1e6d04c8f
kevinoskar <47488256+kevinoskar@users.noreply.github.com>
2021-08-18 22:39:00 -0600

    [ADD] Login

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,29 +1,83 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { CookieService } from 'ngx-cookie-service';
+import { AuthService } from '../services/auth.service';
 
 @Component({
   selector: 'app-login',
-  templateUrl: './login.component.html'
+  templateUrl: './login.component.html',
 })
 export class LoginComponent implements OnInit {
+  
   formLogin = new FormGroup({
-    email: new FormControl('', [
+    correo: new FormControl('', [
       Validators.required,
       Validators.pattern(
         /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i
       ),
     ]),
-    password: new FormControl('', [Validators.required, Validators.minLength(8)]),
+    password: new FormControl('', [
+      Validators.required,
+      Validators.minLength(8),
+    ]),
   });
+  responseLoggin: any = '';
 
-  constructor() { }
+  constructor(
+    private authService: AuthService,
+    private cookiesService: CookieService,
+    private _route: Router
+  ) {}
 
   ngOnInit(): void {
     document.getElementById('body').style.backgroundColor = '#FFE9C7';
   }
 
-  get email() {
-    return this.formLogin.get('email');
+  login() {
+    console.log('El usuario a autenticar:', this.formLogin);
+    this.authService.login(this.formLogin.value).subscribe(
+      (result) => {
+        if (result.code == 1) {
+          if (result.usuario.tipoUsuario.idUsuario == 3) {  
+            const dateNow = new Date();
+            dateNow.setMinutes(dateNow.getMinutes() + 60);
+            this.cookiesService.set('nanyAdminId', result.usuario._id, dateNow);
+            this.cookiesService.set(
+              'nanyAdminNombre',
+              result.usuario.nombre,
+              dateNow
+            );
+            this.cookiesService.set(
+              'nanyAdminApellido',
+              result.usuario.apellido,
+              dateNow
+            );
+            console.log('Logeado con exito');
+            this._route.navigate(['/Categorias']);
+          } else {
+            this.password.setValue('');
+            this.correo.setValue('');
+          }
+        } else if (result.code == 2) {
+          // Contrasenia Incorrecta
+          this.responseLoggin = result;
+          this.password.setValue('');
+        } else {
+          // Correo Incorrecto
+          this.responseLoggin = result;
+          this.correo.setValue('');
+        }
+        console.log(result.message);
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
+  }
+
+  get correo() {
+    return this.formLogin.get('correo');
   }
 
   get password() {
--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, Input } from '@angular/core';
 import { MenuItem, MessageService } from 'primeng/api';
 import { Router } from '@angular/router'
 import { ConfirmationService } from 'primeng/api';
+import { CookieService } from 'ngx-cookie-service';
 
 @Component({
   selector: 'app-navbar',
@@ -11,14 +12,26 @@ export class NavbarComponent implements OnInit {
   items: MenuItem[];
   activeItem: MenuItem;
   @Input() item: number;
+  islogged: boolean;
+  usuario: any = '';
 
   constructor(
     private _route: Router,
     private confirmationService: ConfirmationService,
+    private cookiesService: CookieService,
     private messageService: MessageService
   ) {}
 
   ngOnInit() {
+    if (this.cookiesService.check('nanyAdminId')) {
+      this.islogged = true;
+      this.usuario = {
+        _id: this.cookiesService.get('nanyAdminId'),
+        nombre: this.cookiesService.get('nanyAdminNombre'),
+        apellido: this.cookiesService.get('nanyAdminApellido'),
+      }
+    }
+
     this.items = [
       {
         label: 'Empresas',
@@ -40,7 +53,7 @@ export class NavbarComponent implements OnInit {
         icon: 'pi pi-fw pi-calendar',
         routerLink: ['/Ordenes'],
       },
-      { label: 'Nombre Usuario', icon: 'pi pi-fw pi-user' },
+      { label: this.usuario.nombre + ' ' + this.usuario.apellido , icon: 'pi pi-fw pi-user' },
       {
         icon: 'pi pi-power-off',
         command: (event) => {
@@ -50,6 +63,12 @@ export class NavbarComponent implements OnInit {
             header: 'Confirmar',
             icon: 'pi pi-exclamation-triangle',
             accept: () => {
+              this.cookiesService.delete('nanyAdminId');
+              this.cookiesService.delete('nanyAdminNombre');
+              this.cookiesService.delete('nanyAdminApellido');
+              this.usuario = '';
+              this.islogged = false;
+              console.log('Cerrar Sesion');
               this._route.navigate(['/']);
             },
           });
